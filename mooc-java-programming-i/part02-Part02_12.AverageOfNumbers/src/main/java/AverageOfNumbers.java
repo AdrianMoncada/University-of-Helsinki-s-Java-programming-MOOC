7ed7ecd60e06d625c5c28c1eaa9fdd954430adf4
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-02-15 21:52:38 -0300

    update

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,21 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int amount = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum = sum + number;
+            amount = amount + 1;
+        }
+
+        System.out.println("Average of the numbers:" + (sum / amount));
     }
 }
