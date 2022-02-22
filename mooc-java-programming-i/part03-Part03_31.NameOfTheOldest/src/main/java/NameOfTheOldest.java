955cf074aa6954abacd5b381127414d8151e3bff
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-02-21 22:13:49 -0300

    update

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,27 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int oldest = 0;
+        String nameOfOldest = " ";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
 
+            String[] pieces = input.split(",");
+
+            int number = Integer.valueOf(pieces[1]);
+
+            if (oldest < number) {
+                oldest = number;
+                nameOfOldest = pieces[0];
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + nameOfOldest);
 
     }
 }
